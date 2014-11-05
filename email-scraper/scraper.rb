ab16529673a2c9ec71aeba819d6b7cd594d214af
Zach Latta <zach@zachlatta.com>
2014-11-05 02:38:39 -0800

    Revert "Revert "limit crawl depth to 5.""
    This reverts commit e2ece0e87b7ad059caf818e280d9cf9edafad4ca.

--- email-scraper/scraper.rb
+++ email-scraper/scraper.rb
@@ -4,7 +4,8 @@ EMAIL_REGEX = /[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}/i
 
 ARGF.each do |line|
   begin
-    Anemone.crawl(URI(line), discard_page_bodies: true) do |anemone|
+    Anemone.crawl(URI(line), discard_page_bodies: true,
+                  depth_limit: 5) do |anemone|
       anemone.on_every_page do |page|
         page.body.scan(EMAIL_REGEX).each do |email|
           puts "#{page.url} #{email}"
